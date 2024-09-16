f0b928a65138a07207f20cf622b0ee6d6a7be257
Ilyas Rashidov <rashidov.ilias@gmail.com>
2024-09-16 11:59:48 +0300

    add footer

--- src/entitites/product/ui/Product.jsx
+++ src/entitites/product/ui/Product.jsx
@@ -1,17 +1,19 @@
 import { Image } from "antd"
 import { Star } from "lucide-react"
+import {Divider} from "antd"
 
 export const Product = () => {
     return (
-        <div className="rounded-2xl overflow-hidden w-fit transition duration-300 hover:shadow-xl bg-purple-100 h-120">
-            <Image width={300} height={230} className="object-contain" src="https://img.05.ru/resize/8Z6RGMIm65Yg8U75PPfaqcfIq5DYmeyfO8GzPC90Zsk//rs:fit:500:500:0:0/q:80/bG9jYWw6Ly8vdXBsb2FkL2libG9jay8wMDAwMDIyNTM3Mi82LmpwZw"/>
+        <div className="rounded-2xl overflow-hidden w-fit transition duration-300 hover:shadow-xl bg-white h-120 cursor-pointer ease-in-out hover:scale-105 active:scale-100 active:shadow-none">
+                <Image width={300} height={230} className="object-contain" src="https://img.05.ru/resize/8Z6RGMIm65Yg8U75PPfaqcfIq5DYmeyfO8GzPC90Zsk//rs:fit:500:500:0:0/q:80/bG9jYWw6Ly8vdXBsb2FkL2libG9jay8wMDAwMDIyNTM3Mi82LmpwZw"/>
+            <Divider className="my-0 py-0"/>
             <div className="px-4 py-4 grid gap-1 max-w-64 line-clamp-3">
                 <p className="text-base ">Аккумулятор для Rombica myBook Eclipse</p>
                 <div className="flex gap-1 items-center">
                     <Star size={20} className="text-yellow-500"/>
                     <p className="text-yellow-500">0.0</p>
                 </div>
-                <p className="text-green-700">В наличии</p>
+                <p className="text-green-600">В наличии</p>
                 <p className="font-semibold text-xl">800.00₽</p>
             </div>
         </div>
--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,7 +1,50 @@
+import { Divider } from "antd"
+import { Link } from "react-router-dom"
+import styled from './footer.module.css'
+
+const headTextStyle = {
+    fontSize: '20px',
+    fontWeight: '700'
+}
+
 export const Footer = () => {
     return (
         <div>
-            dsas
+            <Divider className="mb-0"/>
+            <div className="px-3 md:px-10 py-4 flex justify-between font items-start max-md:flex-col gap-3">
+                <div className="grid gap-1">
+                    <p style={headTextStyle}>Моя учетная запись</p>
+                    <Link className={styled.linkTextStyle}>Войти</Link>
+                    <Link className={styled.linkTextStyle}>Создать учетную запись</Link>
+                </div>
+
+                <div className="grid gap-1">
+                    <p style={headTextStyle}>Информация</p>
+                    <Link className={styled.linkTextStyle}>О нас</Link>
+                    <Link className={styled.linkTextStyle}>Обратная связь</Link>
+                    <Link className={styled.linkTextStyle}>Подарочные сертификаты</Link>
+                    <Link className={styled.linkTextStyle}>Торговые марки</Link>
+                    <Link className={styled.linkTextStyle}>Карта сайта</Link>
+                    <Link className={styled.linkTextStyle}>Блог</Link>
+                </div>
+
+                <div className="grid gap-1">
+                    <p style={headTextStyle}>Покупателю</p>
+                    <Link className={styled.linkTextStyle}>Ваши заказы</Link>
+                    <Link className={styled.linkTextStyle}>Отложенные</Link>
+                    <Link className={styled.linkTextStyle}>Список сравнения</Link>
+                </div>
+
+                <div className="grid gap-1">
+                    <p style={headTextStyle}>Контакты</p>
+                    <p>г. Москва, ул. Флотская, д. 6/21, цокольный этаж</p>
+                    <Link className={styled.linkTextStyle}>+7(495)369-21-42</Link>
+                    <p>Пн-Пт 11.00 - 20.00</p>
+                    <Link className={styled.linkTextStyle}>sales@onlinestore.su</Link>
+                    <Link className={styled.linkTextStyle}>Посмотреть на карте</Link>
+                </div>
+            </div>
+            <p className="px-10">© 2016 - 2024 ONLINESTORE. Всегда высокое качество с</p>
         </div>
     )
 }
